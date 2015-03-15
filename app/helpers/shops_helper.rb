2f277bc5dc2382fcf8944eb9a010d8b6fa12945e
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-15 13:47:03 +0900

    Generate shops controller
        % padrino g controller shops get:index get:show

---
+++ app/helpers/shops_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module BeOpenNow
+  class App
+    module ShopsHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers ShopsHelper
+  end
+end
