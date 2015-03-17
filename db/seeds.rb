a3f2c43e28c3b0df3aa8328716828082859d5a8a
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-17 23:38:15 +0900

    Follow data format change
    New format:
    
      * "7:00-23:00"
      * "11:00-15:00, 17:30-21:30"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
 require "yaml"
 require "ostruct"
 
-def add(bithour, first_time, last_time, wday)
-  return unless first_time
-  first_hour = first_time.split(":")[0].to_i
-  last_hour = last_time.split(":")[0].to_i - 1
-  first_hour.upto(last_hour) do |hour|
-    bithour[24 * wday + hour] = "1"
+def add(bithour, open_times, wday)
+  return unless open_times
+  return if open_times.empty?
+  open_times.split(/\s*,\s*/).each do |open_time|
+    first_time, last_time = open_time.split("-")
+    first_hour = first_time.split(":")[0].to_i
+    last_hour = last_time.split(":")[0].to_i - 1
+    first_hour.upto(last_hour) do |hour|
+      bithour[24 * wday + hour] = "1"
+    end
   end
 end
 
@@ -17,32 +21,16 @@ shops.each do |shop_hash|
 
   1.upto(5) do |wday|
     add(bithour,
-        shop.weekday["opening_time"],
-        shop.weekday["closing_time"],
+        shop.weekday,
         wday)
-
-    if shop.weekday["opening_time2"] && shop.weekday["closing_time2"]
-      add(bithour,
-          shop.weekday["opening_time"],
-          shop.weekday["closing_time"],
-          wday)
-    end
   end
 
   add(bithour,
-      shop.saturday["opening_time"],
-      shop.saturday["closing_time"],
+      shop.saturday,
       6)
-  if shop.saturday["opening_time2"] && shop.saturday["closing_time2"]
-    add(bithour,
-        shop.saturday["opening_time2"],
-        shop.saturday["closing_time2"],
-        6)
-  end
 
   add(bithour,
-      shop.sunday["opening_time"],
-      shop.sunday["closing_time"],
+      shop.sunday,
       0)
 
   shop = OpenStruct.new(shop_hash)
